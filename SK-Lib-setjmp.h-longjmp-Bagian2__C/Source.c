257a684b2bd384b58960c8dff406cc7c643d5bf1
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-09-18 10:54:58 +0700

    Add project files.

---
+++ SK-Lib-setjmp.h-longjmp-Bagian2__C/Source.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <setjmp.h>
+#include <stdnoreturn.h>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+jmp_buf jump_buffer;
+
+noreturn void a(int count) {
+    printf("a(%d) called\n", count);
+    longjmp(jump_buffer, count + 1); // will return count+1 out of setjmp
+}
+
+int main(void) {
+    volatile int count = 0; // local vars must be volatile for setjmp
+
+    if (setjmp(jump_buffer) != 9) {
+        a(count++);
+    }
+  
+    _getch();
+}
